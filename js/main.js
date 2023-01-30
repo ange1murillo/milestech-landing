218eeadfe947d13d7c4624b68aef192bf4795e90
Zombie Bytes <>
2023-01-30 07:51:16 -0600

    added lazy load

--- js/main.js
+++ js/main.js
@@ -24,33 +24,63 @@ document.body.addEventListener("click", (event) => {
   }
 });
 
-const faders = document.querySelectorAll('.fade-in');
-const sliders = document.querySelectorAll('.slide-in');
+// const faders = document.querySelectorAll('.fade-in');
+// const sliders = document.querySelectorAll('.slide-in');
+
+// const animateOptions = {
+//   threshold: 0,
+//   rootMargin: "0px 0px -250px 0px"
+// };
+
+// const animateOnScroll = new IntersectionObserver(function (
+//   entries,
+//   animateOnScroll
+// ) {
+//   entries.forEach(entry => {
+//     if (!entry.isIntersecting){
+//       return;
+//     }else{
+//       entry.target.style.willChange = "opacity, transform";
+//       entry.target.classList.add("animate");
+//       animateOnScroll.unobserve(entry.target);
+//     }
+//   });
+// },
+// animateOptions); 
+
+// faders.forEach(fader => {
+//   animateOnScroll.observe(fader);
+// })
+
+// sliders.forEach((slider) => {
+//   animateOnScroll.observe(slider);
+// });
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
 
 const animateOptions = {
   threshold: 0,
-  rootMargin: "0px 0px -250px 0px"
+  rootMargin: "0px 0px -250px 0px",
 };
 
-const animateOnScroll = new IntersectionObserver(function (
-  entries,
-  animateOnScroll
-) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting){
+const animateOnScroll = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
       return;
-    }else{
-      entry.target.style.willChange = "opacity, transform";
-      entry.target.classList.add("animate");
-      animateOnScroll.unobserve(entry.target);
+    } else {
+      requestAnimationFrame(() => {
+        entry.target.style.willChange = "opacity, transform";
+        entry.target.classList.add("animate");
+        animateOnScroll.unobserve(entry.target);
+      });
     }
   });
-},
-animateOptions); 
+}, animateOptions);
 
-faders.forEach(fader => {
+faders.forEach((fader) => {
   animateOnScroll.observe(fader);
-})
+});
 
 sliders.forEach((slider) => {
   animateOnScroll.observe(slider);
